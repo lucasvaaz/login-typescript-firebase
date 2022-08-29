f895a5d21cac4324f4b0b29624c4bfca2262cc10
Lucas Vaz <96131635+lucasvaaz@users.noreply.github.com>
2022-08-29 00:48:16 -0300

    :sparkles: feat: add imagem de acordo com o theme na tela de login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,12 +1,19 @@
 import { useContext } from "react";
 import { ChooseThemeContext } from "../../context/chooseTheme";
 
-import { Wrapper } from "./styled";
+import { ImgContainer, Wrapper } from "./styled";
+
+import imgDark from '../../assets/imgDark.jpg'
+import imgLight from '../../assets/imgLight.jpg'
 
 export function Login() {
   const { isDarkTheme, setIsDarkTheme } = useContext(ChooseThemeContext);
   return (
     <Wrapper>
+      <ImgContainer style={{ backgroundImage: `url(${isDarkTheme ? imgDark : imgLight})`, backgroundPosition: 'center', backgroundSize: 'cover' }}>
+        
+      </ImgContainer>
+
       <button onClick={() => {
         setIsDarkTheme(!isDarkTheme)
       }}> tema </button>
--- src/pages/login/styled.ts
+++ src/pages/login/styled.ts
@@ -1,9 +1,17 @@
 import styled from "styled-components";
 
+
+
 export const Wrapper = styled.div`
     width: 100%; 
     height: 100vh;
     background-color: ${props => props.theme.background};
     color: ${props => props.theme.primaryText};
-    padding: 20px;
 ` 
+export const ImgContainer = styled.div`
+    width: 50%;
+    height: 100vh;
+
+
+
+`

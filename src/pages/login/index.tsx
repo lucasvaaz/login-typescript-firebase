a18cf6a6d9591fa9d7a658dd25bb7d90f7be381d
Lucas Vaz <96131635+lucasvaaz@users.noreply.github.com>
2022-08-29 23:03:08 -0300

    :sparkles: feat: criação do header login, com switch dark/light theme

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,22 +1,42 @@
 import { useContext } from "react";
 import { ChooseThemeContext } from "../../context/chooseTheme";
 
-import { ImgContainer, Wrapper } from "./styled";
+import { DarkIcon, Header, IconLogo, ImgContainer, LightIcon, LoginContainer, Wrapper } from "./styled";
 
-import imgDark from '../../assets/imgDark.jpg'
-import imgLight from '../../assets/imgLight.jpg'
+import imgDark from "../../assets/imgDark.jpg";
+import imgLight from "../../assets/imgLight.jpg";
+
+
+
+
 
 export function Login() {
   const { isDarkTheme, setIsDarkTheme } = useContext(ChooseThemeContext);
   return (
     <Wrapper>
-      <ImgContainer style={{ backgroundImage: `url(${isDarkTheme ? imgDark : imgLight})`, backgroundPosition: 'center', backgroundSize: 'cover' }}>
-        
-      </ImgContainer>
+      <ImgContainer
+        style={{
+          backgroundImage: `url(${isDarkTheme ? imgDark : imgLight})`,
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+        }}
+      ></ImgContainer>
 
-      <button onClick={() => {
-        setIsDarkTheme(!isDarkTheme)
-      }}> tema </button>
+      <LoginContainer>
+
+          <Header>
+            <div>
+            <IconLogo /> 
+            <h2>MOUNTAIN ADVENTURE</h2>
+            </div>
+            <span onClick={() => setIsDarkTheme(!isDarkTheme)}>
+            {isDarkTheme ? <LightIcon /> : <DarkIcon />}
+            </span>
+          </Header>
+        
+      </LoginContainer>
+
+      
     </Wrapper>
   );
 }
--- src/pages/login/styled.ts
+++ src/pages/login/styled.ts
@@ -1,17 +1,80 @@
 import styled from "styled-components";
-
-
+import { GiMountains } from "react-icons/gi";
+import { MdDarkMode, MdLightMode } from "react-icons/md";
 
 export const Wrapper = styled.div`
-    width: 100%; 
-    height: 100vh;
-    background-color: ${props => props.theme.background};
-    color: ${props => props.theme.primaryText};
-` 
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  background-color: ${(props) => props.theme.background};
+  color: ${(props) => props.theme.primaryText};
+`;
 export const ImgContainer = styled.div`
-    width: 50%;
-    height: 100vh;
+  width: 50%;
+  height: 100vh;
+`;
+
+export const LoginContainer = styled.main`
+  padding: 5rem;
+  width: 50%;
+`;
+
+export const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  div { 
+    display: flex;
+    align-items: center;
+    
+
+    h2 { 
+        color: ${(props) => props.theme.primaryText};
+        margin-left: 0.5rem;
+    }
+  }
+
+  span { 
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 9999px;
+    box-shadow: 1px 1px ;
+    background-color: ${(props) => props.theme.secundaryLight};
+  }
+`;
 
 
 
-`
+////////////////////////////ICONS//////////////////////////////
+
+export const IconLogo = styled(GiMountains)`
+  font-size: 3rem;
+  color: ${(props) => props.theme.primaryText};
+`;
+export const DarkIcon = styled(MdDarkMode)`
+  font-size: 2rem;
+  color: ${(props) => props.theme.primaryText};
+  cursor: pointer;
+  padding: 0.3rem;
+  transition: 300ms;
+
+  &:hover {
+    color: ${(props) => props.theme.secundaryDark};
+  }
+`;
+
+export const LightIcon = styled(MdLightMode)`
+  font-size: 2rem;
+  color: ${(props) => props.theme.primaryText};
+  cursor: pointer;
+  padding: 0.3rem;
+  transition: 300ms;
+
+  &:hover {
+    color: ${(props) => props.theme.secundaryDark};
+  }
+`;
+
+////////////////////////////ICONS//////////////////////////////

9dc96b7a0f21838dd7de9cb74b3d3de5e6e08a88
Lucas Vaz <luucaasvaz@gmail.com>
2022-09-12 23:30:25 -0300

    :sparkles: feat: criação da página register

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,9 +1,36 @@
+import { useState } from "react";
+import { FormRegister } from "./styled";
 
+export function Register() {
+  const [showPassword, setShowPassword] = useState(true)
+  return (
+    <>
+      <div>
+        <h1>register</h1>
+      </div>
+      <FormRegister>
+        <label htmlFor="name">Nome:</label>
+        <input id="name" type="text" />
 
-export function Register(){
-    return (
-       <div>
-        <h1>Register</h1>
-       </div>
-    )   
+        <label htmlFor="lastName">Sobrenome:</label>
+        <input id="LastName" type="text" />
+
+        <label htmlFor="email">Email:</label>
+        <input id="email" type="text" />
+
+        <label htmlFor="password">Senha:</label>
+        <input id="password" type="password" />
+
+        <label htmlFor="passwordConfirm">Confirmar senha:</label>
+        <input id="passwordConfirm" type="password" />
+       
+      
+        
+
+        <div>
+          <button type="submit"> CADASTRAR </button>
+        </div>
+      </FormRegister>
+    </>
+  );
 }
--- src/pages/register/styled.ts
+++ src/pages/register/styled.ts
@@ -1,2 +1,84 @@
 import styled from "styled-components";
 
+export const FormRegister = styled.form`
+  input[type=text] {
+    width: 100%;
+    height: 2.5rem;
+    font-size: 1rem;
+    font-weight: 200;
+    color: ${(props) => props.theme.inputText};
+    outline: none;
+    border: none;
+    border-radius: 0.4rem;
+    padding: 0.7rem;
+    border: 2px solid ${(props) => props.theme.secundaryText};
+
+    &:focus {
+      outline: 2px solid ${(props) => props.theme.secundaryLight};
+      border: none;
+    }
+  }
+
+  input[type=password] {
+    width: 100%;
+    height: 2.5rem;
+    font-size: 1rem;
+    font-weight: 200;
+    color: ${(props) => props.theme.inputText};
+    outline: none;
+    border: none;
+    border-radius: 0.4rem;
+    padding: 0.7rem;
+    border: 2px solid ${(props) => props.theme.secundaryText};
+
+    &:focus {
+      outline: 2px solid ${(props) => props.theme.secundaryLight};
+      border: none;
+    }
+  }
+
+ 
+
+  button {
+    width: 100%;
+    height: 3rem;
+    font-size: 1.3rem;
+    font-weight: 800;
+    font-family: "Nunito";
+    color: ${(props) => props.theme.secundaryText};
+    margin-top: 1.5rem;
+    background-color: ${(props) => props.theme.secundaryLight};
+    border: none;
+    border-radius: 0.4rem;
+    cursor: pointer;
+    transition: 500ms;
+
+    &:disabled {
+      cursor: not-allowed;
+      background-color: ${(props) => props.theme.secundaryDark};
+      color: ${(props) => props.theme.buttonText};
+    }
+
+    &:not(:disabled):hover {
+      background-color: ${(props) => props.theme.secundaryDark};
+    }
+  }
+
+  label {
+    display: block;
+    font-weight: 300;
+    padding: 2rem 0 0 0;
+  }
+
+  a {
+    text-decoration: none;
+    font-weight: 500;
+    color: ${(props) => props.theme.secundaryLight};
+
+    &:hover {
+      color: ${(props) => props.theme.secundaryDark};
+    }
+  }
+ 
+
+`;

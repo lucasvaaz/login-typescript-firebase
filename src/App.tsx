476e87d9672067a01ccd14380b2813930c3058e1
Lucas Vaz <luucaasvaz@gmail.com>
2022-09-12 22:08:12 -0300

    :sparkles: feat: criação das rotas login e register

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,19 @@ import { Login } from "./pages/login";
 import { GlobalStyle } from "./styles/global";
 import { darkTheme } from "./styles/themes/dark";
 import { lightTheme } from "./styles/themes/light";
+import { BrowserRouter } from "react-router-dom"
+import { Register } from "./pages/register";
+import { Router } from "./Router";
 
 function App() {
   const { isDarkTheme } = useContext(ChooseThemeContext);
 
   return (
     <ThemeProvider theme={isDarkTheme ? darkTheme : lightTheme}>
-      <Login />
+     <BrowserRouter>
+      
+      <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   );
---
+++ src/Router.tsx
@@ -0,0 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import { Login } from "./pages/login";
+import { Register } from "./pages/register";
+
+
+export function Router() {
+
+    return (
+        <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+        </Routes>
+    )
+}

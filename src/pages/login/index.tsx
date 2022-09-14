54f76f93e9fbd0f640155d066bd5db12efcc2d17
Lucas Vaz <luucaasvaz@gmail.com>
2022-09-14 01:02:40 -0300

    :sparkles: feat: add verificação com zod

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,4 +1,12 @@
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as zod from "zod";
+
+interface ShowModalProps {
+  children: React.ReactNode | React.ReactNode[];
+}
+
 import {
   Divider,
   FacebookIcon,
@@ -8,22 +16,51 @@ import {
   Register,
 } from "./styled";
 
+const newValidationSchema = zod.object({
+  emailLogin: zod.string().email({ message: "Email invalido" }),
+});
+
 export function Login() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(newValidationSchema),
+  });
+
+  function handleSubmitLogin(data: any) {
+    console.log(data);
+  }
+
+  const loginAndPassword = watch("emailLogin" && "passwordLogin");
+  const isSubmitDisabled = !loginAndPassword;
   return (
     <>
       <div>
         <h1>login</h1>
       </div>
-      <FormLogin>
-        <label htmlFor="email">email</label>
-        <input id="email" type="text" />
 
-        <label htmlFor="password">senha</label>
-        <input id="password" type="password" />
+      <FormLogin onSubmit={handleSubmit(handleSubmitLogin)}>
+        <label htmlFor="emailLogin">email</label>
+        <input id="emailLogin" type="text" {...register("emailLogin")} />
+
+        {errors.emailLogin && <h5> {errors.emailLogin?.message} </h5>}
+
+        <label htmlFor="passwordLogin">senha</label>
+        <input
+          id="passwordLogin"
+          type="password"
+          {...register("passwordLogin")}
+        />
         <a href="#">Esqueci minha senha</a>
 
         <div>
-          <button type="submit"> ENTRAR </button>
+          <button type="submit" disabled={isSubmitDisabled}>
+            {" "}
+            ENTRAR{" "}
+          </button>
         </div>
       </FormLogin>
       <Register>

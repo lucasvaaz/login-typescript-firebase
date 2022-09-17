21fab8197bea0371f560a7b4b1cefadf320fc6cd
Lucas Vaz <luucaasvaz@gmail.com>
2022-09-17 02:13:37 -0300

    :sparkles: feat: validação com zod na pagina de registro

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,28 +1,60 @@
-import { useState } from "react";
+
 import { FormRegister } from "./styled";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as zod from "zod"
+
+const newValidationSchema = zod.object({
+  name: zod.string().min(3),
+  lastName: zod.string().min(2),
+  email: zod.string().email(),
+  password: zod.string().min(6),
+  passwordConfirm: zod.string().min(6),
+  
+})
 
 export function Register() {
   
+ const {
+  register,
+  handleSubmit,
+  watch,
+  formState: { errors },
+ } = useForm({
+  resolver: zodResolver(newValidationSchema),
+  defaultValues: { 
+    name: "",
+    lastName: "",
+    email: "",
+    password: "",
+    passwordConfirm: "",
+ }
+})
+
+function handleSubmitRegister(data: any) {
+  console.log(data)
+}
+
   return (
     <>
       <div>
         <h1>cadastrar</h1>
       </div>
-      <FormRegister>
+      <FormRegister onSubmit={handleSubmit(handleSubmitRegister)}>
         <label htmlFor="name">Nome:</label>
-        <input id="name" type="text" />
+        <input id="name" type="text" {...register("name")} />
 
         <label htmlFor="lastName">Sobrenome:</label>
-        <input id="LastName" type="text" />
+        <input id="lastName" type="text" {...register("lastName")} />
 
         <label htmlFor="email">Email:</label>
-        <input id="email" type="text" />
+        <input id="email" type="text" {...register("email")} />
 
         <label htmlFor="password">Senha:</label>
-        <input id="password" type="password" />
+        <input id="password" type="password" {...register("password")} />
 
         <label htmlFor="passwordConfirm">Confirmar senha:</label>
-        <input id="passwordConfirm" type="password" />
+        <input id="passwordConfirm" type="password" {...register("passwordConfirm")} />
        
       
         
